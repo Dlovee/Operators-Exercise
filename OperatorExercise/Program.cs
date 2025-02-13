8b44d2641e1a7784cf60846db914af208fd02e55
Dlove <dlove327@gmail.com>
2025-02-12 19:36:37 -0600

    Completed the Operator excercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,37 @@
 {
     public class Program
     {
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static void PrintAreaOfCircle()
+        {
+            Console.WriteLine("What is the area of the circle?");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of the circle with a radius of {radius} is {Math.Round(AreaOfCircle(radius), 3)}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int subtraction = a - b;
+            int multiplication = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            //Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            
+            //var is implicit type inferrence (more on this later);
+            int i = 3;
+            int j = 4;
+            int k = ++i * j++;
+
+            Console.WriteLine(k); // What will this output?
         }
     }
 }
